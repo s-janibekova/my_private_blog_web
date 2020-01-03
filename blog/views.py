43fdd09e313091c97e4d960d950c743fda85c917
S_Janybekova <s.t.janibekova@gmail.com>
2020-01-04 01:28:37 +0600

    style of blog

--- blog/views.py
+++ blog/views.py
@@ -15,4 +15,4 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
-    return render(request, 'blog/post/detail,html', {'post': post})
+    return render(request, 'blog/post/detail.html', {'post': post})
