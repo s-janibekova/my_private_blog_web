39db14c8ba733963266b0c9d8b53d6e61ba7f65e
S_Janybekova <s.t.janibekova@gmail.com>
2020-01-09 19:01:46 +0600

    comments functionality is added:

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Post
+from .models import Comment
 
 # Register your models here.
 
@@ -13,3 +14,9 @@ class PostAdmin(admin.ModelAdmin):
     raw_id_fields = ('author',)
     date_hierarchy = 'publish'
     ordering = ('status', 'publish')
+
+@admin.register(Comment)
+class Comment(admin.ModelAdmin):
+    list_display = ('name', 'email', 'created', 'post', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'body', 'post', 'email')
--- blog/models.py
+++ blog/models.py
@@ -11,6 +11,7 @@ class PublishedManager(models.Manager):
 
 # Create your models here.
 
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
@@ -40,3 +41,20 @@ class Post(models.Model):
         return reverse('blog:post_detail',
                        args=[self.publish.year, self.publish.month,
                              self.publish.day, self.slug])
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True)
+
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return 'Comment by {} on {}'.format(self.name, self.post)
